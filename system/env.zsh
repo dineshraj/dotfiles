aca7d9e352a10bd3be0527bf091d9f9a008fc07d
Dineshraj <dineshraj.goomany@bbc.co.uk>
2014-11-20 17:09:34 +0000

    adds env vars

--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,9 @@
-export EDITOR='atom'
+export EDITOR='subl'
+export SIKULIX_HOME="~/Code/workspace/tip-git/lib/sikuli-java.jar"
+
+# always use color output for ls
+export CLICOLOR=1
+
+# tell grep to highlight matches
+export GREP_OPTIONS='--color=auto'
+
